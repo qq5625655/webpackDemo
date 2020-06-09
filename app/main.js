dd33507d1ac79f1515fa2113244c6449d3f3be5d
zhangxusheng <zhangxusheng@51shebao.com>
2020-06-09 22:33:29 +0800

    webpack中babel与babel-runtime之间的关系

--- app/main.js
+++ app/main.js
@@ -1,14 +1,14 @@
 // main.js
 import App from './App';
 import Vue from 'vue';
-import router from '../router/index.js'
-import store from '../store/index.js'
+// import router from '../router/index.js'
+// import store from '../store/index.js'
 
 
 new Vue({
   el:'#root',
-  router,
-  store,
+  // router,
+  // store,
   components: {App},
   template:'<App/>'
 })
